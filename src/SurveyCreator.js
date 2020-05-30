c271e076d617cb70f3489d8dd1181767abf5cfb1
marcziss <65080578+marcziss@users.noreply.github.com>
2020-05-30 17:54:42 -0400

    tried  to use ckeditor as a property editor like in sample

--- src/SurveyCreator.js
+++ src/SurveyCreator.js
@@ -3,6 +3,10 @@ import * as SurveyJSCreator from "survey-creator";
 import * as SurveyKo from "survey-knockout";
 import "survey-creator/survey-creator.css";
 
+//ADDED THESE 2 
+import * as CKEDITOR from 'ckeditor';
+import * as showdown from "showdown";
+
 import "jquery-ui/themes/base/all.css";
 import "nouislider/distribute/nouislider.css";
 import "select2/dist/css/select2.css";
@@ -37,6 +41,63 @@ widgets.ckeditor(SurveyKo);
 widgets.autocomplete(SurveyKo, $);
 widgets.bootstrapslider(SurveyKo);
 
+
+///ADDED THIS CODE BLOCK FROM SAMPLE
+
+var CkEditor_ModalEditor = {
+  afterRender:  (modalEditor, htmlElement) => {   
+      var editor = CKEDITOR.replace(htmlElement);
+      var isUpdating = false;
+      editor.on("change", function () {
+          isUpdating = true;
+          modalEditor.editingValue = editor.getData();
+          isUpdating = false;
+      }
+      );
+      editor.setData(modalEditor.editingValue);
+      modalEditor.onValueUpdated = function (newValue) {
+          if (!isUpdating) {
+              editor.setData(newValue);
+          }
+      };
+  },
+  destroy: (modalEditor, htmlElement) => {
+      var instance = CKEDITOR.instances[htmlElement.id];
+      if (instance) {
+          instance.removeAllListeners();
+          CKEDITOR.remove(instance);
+      }
+  }
+};
+
+  SurveyJSCreator
+  .SurveyPropertyModalEditor
+  .registerCustomWidget("html", CkEditor_ModalEditor);
+  
+  SurveyJSCreator
+  .SurveyPropertyModalEditor
+  .registerCustomWidget("text", CkEditor_ModalEditor);
+  
+  
+  
+  //Create showdown mardown converter
+  var converter = new showdown.Converter();
+  function doMarkdown(survey, options) {
+  //convert the mardown text to html
+  var str = converter.makeHtml(options.text);
+  if (str.indexOf("<p>") == 0) {
+      //remove root paragraphs<p></p>
+      str = str.substring(3);
+      str = str.substring(0, str.length - 4);
+  }
+  //set html
+  options.html = str;
+  }
+
+///
+
+
+
 class SurveyCreator extends Component {
   surveyCreator;
   componentDidMount() {
@@ -45,6 +106,32 @@ class SurveyCreator extends Component {
       null,
       options
     );
+
+
+/// ADDED THIS Could block from sample
+    this.surveyCreator
+    .survey
+    .onTextMarkdown
+    .add(doMarkdown);
+    this.surveyCreator
+    .onDesignerSurveyCreated
+    .add(function (editor, options) {
+        options
+            .survey
+            .onTextMarkdown
+            .add(doMarkdown);
+    });
+    this.surveyCreator
+    .onTestSurveyCreated
+    .add(function (editor, options) {
+        options
+            .survey
+            .onTextMarkdown
+            .add(doMarkdown);
+    });
+
+///
+
     this.surveyCreator.saveSurveyFunc = this.saveMySurvey;
     this.surveyCreator.tabs().push({
       name: "survey-templates",
